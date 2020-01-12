9cb73e2b003f23f6a25b66b56bd92e8aa4d2fb6d
sxhjoker@163.com <water!264835>
2020-01-13 00:09:11 +0800

    1

---
+++ src/com/sxh/newfolder/DP17_责任链模式/HandlerTest.java
@@ -0,0 +1,15 @@
+package com.sxh.newfolder.DP17_责任链模式;
+
+/**
+ * @author 一池春水倾半城
+ * @date 2020/1/13 0:08
+ */
+public class HandlerTest {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 实现一个生产电脑的生产链，一台电脑包括键盘鼠标显示器。
+     */
+}
