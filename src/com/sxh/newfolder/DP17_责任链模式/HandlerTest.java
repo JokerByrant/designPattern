0e6d80df36f13ac461bc06558378af019b17ee98
sxhjoker <sxhjoker@163.com>
2020-01-16 17:04:44 +0800

    责任链模式Demo

--- src/com/sxh/newfolder/DP17_责任链模式/HandlerTest.java
+++ src/com/sxh/newfolder/DP17_责任链模式/HandlerTest.java
@@ -1,15 +1,14 @@
 package com.sxh.newfolder.DP17_责任链模式;
 
+import com.sxh.newfolder.DP17_责任链模式.DP_17.model.NullException;
+
 /**
- * @author 一池春水倾半城
- * @date 2020/1/13 0:08
+ * @author sxh
+ * @date 2020/1/16
  */
 public class HandlerTest {
     public static void main(String[] args) {
-
+        NullException exception = new NullException();
+        exception.filter();
     }
-
-    /**
-     * 实现一个生产电脑的生产链，一台电脑包括键盘鼠标显示器。
-     */
 }
